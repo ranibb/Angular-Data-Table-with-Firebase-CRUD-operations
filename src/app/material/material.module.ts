1a48c4a8a424a018cd2299033600150496c36895
Rani Abu Sultan <raniabusultan@gmail.com>
2018-09-27 15:58:15 +0300

    Project initialization, application structuring and a form UI implementation

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as Material from '@angular/material';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatCheckboxModule,
+    Material.MatButtonModule
+  ],
+  exports: [
+    Material.MatToolbarModule,
+    Material.MatGridListModule,
+    Material.MatFormFieldModule,
+    Material.MatInputModule,
+    Material.MatRadioModule,
+    Material.MatSelectModule,
+    Material.MatDatepickerModule,
+    Material.MatNativeDateModule,
+    Material.MatCheckboxModule,
+    Material.MatButtonModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
